e7774b73c51fcb1e7c18d28ea6522bc4b190f2d7
mveer01 <101559815+mveer01@users.noreply.github.com>
2022-03-30 19:44:15 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,24 @@
+package kar;
+public class calculator {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calculator cal = new calculator();
+    cal.ADD(4,5);
+    cal.SUB(9, 3);
+    
+	}
+
+	public int ADD(int a, int b)
+	{
+		int c= a + b;
+		return c;
+	}
+	public int SUB(int d, int e)
+	{
+	    int f= d - e;	
+	    return f;
+	}
+	
+	
+}
---
+++ module-info.java
@@ -0,0 +1,2 @@
+module Karthik_TEST {
+}
